6b788a9277f4e4eade703529c69efb69ef426caf
Alex Melton <alex@melton.io>
2024-12-07 11:25:13 -0600

    Setting up infra

---
+++ README.md
@@ -0,0 +1,3 @@
+## DNASeq Kinase Analyzer PyMol Plugin
+
+You will need a valid dnaseq email address to use this plugin.
--- dnaseq.py
+++ dnaseq.py
@@ -1,2 +1,12 @@
-from pymol import cmd
+#
+# -- basicCodeBlock.py
+#
+from pymol import cmd, stored
 
+def __init_plugin__(app=None):
+    from pymol.plugins import addmenuitemqt
+    addmenuitemqt('DNASeq Kinase Analyzer', myqtdialog)
+
+def myqtdialog():
+    from pymol.Qt import QtWidgets
+    QtWidgets.QMessageBox.information(None, 'DNASeq Kinase Analyzer', "There's nothing here yet because it's a WIP. There will be though!!")
